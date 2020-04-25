53b0b732b08319cb86ea0e6590fa7b1b28fb50b8
Marenda S.A <renda_ardy@outlook.com>
2020-04-25 10:54:49 +0800

    Remove unused code and dependency

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/app.js",
@@ -11,19 +10,6 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-      //   use: [
-      //     {
-      //       loader: "file-loader",
-      //       options: {
-      //         name: "[name].[ext]",
-      //         outputPath: "/fonts/",
-      //         publicPath: "/static/fonts",
-      //       },
-      //     },
-      //   ],
-      // },
       {
         test: /\.s[ac]ss$/i,
         use: [
@@ -45,12 +31,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "css/style.css",
     }),
-    // new CopyWebpackPlugin([
-    //   {
-    //     from: "./node_modules/@fortawesome/fontawesome-free/webfonts",
-    //     to: "./dist",
-    //   },
-    // ]),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
